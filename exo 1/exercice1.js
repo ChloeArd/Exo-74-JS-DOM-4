824a2606a9c397fd94a768a1fb0aa7848f57111d
ChloeArd <chloe.ardoise@gmail.com>
2020-12-04 11:28:05 +0100

    Exercice 74

--- exo 1/exercice1.js
+++ exo 1/exercice1.js
@@ -1,6 +1,11 @@
 //Votre code ci dessous
+let catPic = document.getElementById('cat-pic');
 
+function changeText() {
+    document.getElementById('cat-chat').innerHTML = "Miaouuuuuu !"
+}
 
+catPic.addEventListener("mousemove", changeText);
 
 
 
--- exo 3/exercice.js
+++ exo 3/exercice.js
@@ -1,10 +1,47 @@
 let libButton = document.getElementById('lib-button');
+
 let libIt = function() {
     let storyDiv = document.getElementById("story");
-    storyDiv.innerHTML = "??Your story here";
+    let noun = document.getElementById('noun');
+    let adjective = document.getElementById('adjective');
+    let name = document.getElementById('person');
+    noun.innerHTML = "éléphant";
+    adjective.innerHTML = "délicieux";
+    name.innerHTML = "Roger";
+    storyDiv.innerHTML = "J'ai manger un(e) " + noun + " c'était " + adjective + " et il s'appelait " + name + " .";
 };
+
 libButton.addEventListener('click', libIt);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+getComputedStyle()
 
 
 
